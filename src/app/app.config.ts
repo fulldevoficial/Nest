3285d8c4dc40b0b406b916220b3dac005d29946f
Lucas Pedro <lucasdearaujopedrolap@gmail.com>
2026-01-25 13:15:58 -0300

    Refactor: Altera SSR para SPA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+// import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { APP_ENVIRONMENT } from '@core/config/app-environment.token';
 import { environment } from '@environments/environment';
 import { provideHttpClient, withFetch } from '@angular/common/http';
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
+    //provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch()),
     { provide: APP_ENVIRONMENT, useValue: environment }
   ]
